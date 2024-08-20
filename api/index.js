b1f84abeecdda86a95be8f34e5c944a259d4841b
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-20 19:53:23 +0545

    create sign up api

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT;
 mongoose.connect(process.env.MONGO)
@@ -18,3 +20,4 @@ mongoose.connect(process.env.MONGO)
     });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
