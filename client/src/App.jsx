1c4f8df391d619434bd68647fe8fadf0e440441f
aklesh yadav <arzunayavadu998@gmail.com>
2024-08-20 13:45:00 +0545

    react-app with tailwind CSS template

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <main className='font-bold text-yellow-400 capitalize'>
+        This is a main App section.
+      </main>
+    </>
+  )
+}
+
+export default App
