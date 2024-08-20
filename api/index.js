0f0de4afb5da4e23826cb9bd3322df10a48a24af
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-20 15:58:14 +0545

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT;
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => console.log(`Sever running on Port: ${PORT}`))
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB:', err);
+    });
 
 app.get('/', (req, res) => {
     res.send('Server running on localhost:3000');
 });
-app.listen(3000, () => console.log('Server running on port: 3000'));
