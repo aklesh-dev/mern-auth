d13f0a29f982263d1b4fedf2c01fa5fffdf07e18
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-23 12:30:37 +0545

    create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,42 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import User from '../models/User.Model.js';
+
+
 export const test = (req, res) => {
     res.json({
         "message": "Api is called from controller."
     })
+};
+
+// --update user
+export const updateUser = async (req, res, next) => {
+    //--check user is autorized
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(403, "You can only update your account."));
+    };
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        // --update the user
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    profilePicture: req.body.profilePicture
+                }
+            },
+            { new: true }
+        );
+        const { password, ...rest } = updatedUser._doc; //remove the password from the response
+        res.status(200).json(rest);
+
+    } catch (error) {
+        next(errorHandler(500, "Error updating user"));
+    }
 };
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 mongoose.connect(process.env.MONGO)
